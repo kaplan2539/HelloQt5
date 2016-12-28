e7b8c3e2cf05e6930e47a68ec0c17d0cc16d182b
Alex Kaplan <kaplan2539@gmail.com>
2016-12-27 23:06:25 -0800

    parametrizing bintray.json

--- build.sh
+++ build.sh
@@ -2,12 +2,26 @@
 
 set -e
 
-OS=$(uname -s)
+export OS=$(uname -s)
+export BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export REPO=$(git remote get-url origin)
+export HTTP_REPO=${REPO//:/\/}
+export HTTP_REPO=${HTTP_REPO//git@/http:\/\/}
+export PROJECT=${REPO##*/}
+export GITUSER=${REPO##*:}; GITUSER=${GITUSER%%/*}
+
+echo "OS=${OS}"
+echo "BRANCH=${BRANCH}"
+echo "REPO=${REPO}"
+echo "HTTP_REPO=${HTTP_REPO}"
+echo "PROJECT=${PROJECT}"
+echo "GITUSER=${GITUSER}"
 
 build() {
 rm -rf build
 mkdir build
 pushd build
+envsubst <../bintray.json.in >bintray.json
 cmake -G Ninja ..
 ninja
 cpack
@@ -31,3 +45,5 @@ case "${OS}" in
 esac
 
 build
+
+
